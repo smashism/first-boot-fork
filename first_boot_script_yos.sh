7a9964155ae17f239ea87163eaed2b35b290fa61
smashism <emily@modtitan.com>
2015-01-06 15:35:58 -0600

    Update first_boot_script_yos.sh
    added mouse/trackpad configurations

--- first_boot_script_yos.sh
+++ first_boot_script_yos.sh
@@ -10,7 +10,7 @@
 #
 # Adapted for use at My Company
 # Emily Kausalik, 10-28-2014
-# Last modified 11-05-2014
+# Last modified 1-06-2015
 
 # Sleeping for 30 seconds to allow the new default User Template folder to be moved into place
 
@@ -273,6 +273,20 @@ fi
 fi
 done
 fi
+
+# enable tap-click on trackpad
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# disable "natural" scrolling
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+# map bottom right corner to right-click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
 
 # Turn SSH on
 
